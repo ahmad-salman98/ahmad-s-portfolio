018dae9f32ebdc87ff18f654544a3bce03c79e5a
Ahmad Odeh <ahmad98.od@gmail.com>
2026-04-13 15:37:57 +0300

    refactor: reorder the location of the files

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,9 +1,7 @@
-import Header from "@/components/layout/header/header";
 
 export default function TestPage() {
     return (
         <div className="min-h-screen bg-gray-100 pt-20 container mx-auto">
-            <Header />
         </div>
     );
 }
