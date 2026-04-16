fa3cbf34ce506872e0287bfeeee7c28a98aab3f2
Ahmad Odeh <ahmad98.od@gmail.com>
2026-04-16 12:28:25 +0300

    style: refine container max-width and padding for better responsiveness

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -2,7 +2,7 @@ import Services from "@/features/services/services";
 
 export default function TestPage() {
     return (
-        <div className="container pt-[100px] mx-auto">
+        <div className="container pt-[100px]">
             <Services/>
         </div>
     );
